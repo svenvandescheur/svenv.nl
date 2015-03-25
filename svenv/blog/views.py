c814654e60b41c01b26f6b024d6c52cc196aaac4
Sven van de Scheur <svenvandescheur@gmail.com>
2015-03-26 00:07:39 +0100

    Add basic index view

--- svenv/blog/views.py
+++ svenv/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from blog.models import Blog
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'blog_list'
+
+    def get_queryset(self):
+        return Blog.objects.order_by('date')
