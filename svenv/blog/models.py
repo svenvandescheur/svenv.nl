003f7337d267746c6a4c643eaf627ad070abd919
Sven van de Scheur <svenvandescheur@gmail.com>
2015-03-25 23:35:54 +0100

    no auto date

--- svenv/blog/models.py
+++ svenv/blog/models.py
@@ -6,4 +6,4 @@ class Blog(models.Model):
 
     title = models.CharField(max_length=200)
     content = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField()
