114b14cb465e325d45ff69dfd478342c56a2ecfe
Sven van de Scheur <svenvandescheur@gmail.com>
2015-03-25 23:22:34 +0100

    Auto date

--- svenv/blog/models.py
+++ svenv/blog/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
