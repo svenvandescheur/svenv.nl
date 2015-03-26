67a700189b5608700239ead85d1cb4b2bf8035ea
Sven van de Scheur <svenvandescheur@gmail.com>
2015-03-26 20:01:07 +0100

    Refactor a bit

--- svenv/blog/urls.py
+++ svenv/blog/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from blog import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.ListView.as_view(), name='list'),
 )
--- svenv/blog/views.py
+++ svenv/blog/views.py
@@ -1,9 +1,14 @@
 from django.views import generic
 from blog.models import Blog
 
-class IndexView(generic.ListView):
+
+class ListView(generic.ListView):
     template_name = 'blog/index.html'
     context_object_name = 'blog_list'
 
     def get_queryset(self):
+        """
+        Gets a list of Blog items in newest first order
+        :return:
+        """
         return Blog.objects.order_by('date').reverse()
