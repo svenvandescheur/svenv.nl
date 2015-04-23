f0746472527128f6f9fd7bbc4679548f6215194c
Sven van de Scheur <svenvandescheur@gmail.com>
2015-04-24 00:21:45 +0200

    Add image support

--- svenv/blog/models.py
+++ svenv/blog/models.py
@@ -2,6 +2,16 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Category(models.Model):
+    """
+    Category model
+    """
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=200)
+
+
 class Post(models.Model):
     """
     Post model
@@ -14,17 +24,21 @@ class Post(models.Model):
 
     author = models.ForeignKey(User)
     category = models.ForeignKey('Category')
+    image = models.ForeignKey('Image')
     title = models.CharField(max_length=200)
     url_title = models.CharField(max_length=100)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
 
-class Category(models.Model):
+class Image(models.Model):
     """
-    Category model
+    Image to be used as header image in blog post
     """
     def __str__(self):
-        return self.name
+        return self.title
 
-    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=200)
+    width = models.IntegerField()
+    height = models.IntegerField()
