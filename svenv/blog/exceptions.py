4016bd64ab751a438460268e4b63a5f282186cc7
Sven van de Scheur <svenvandescheur@gmail.com>
2015-04-23 08:53:30 +0200

    Remove check

--- svenv/blog/exceptions.py
+++
@@ -1,5 +0,0 @@
-class NotUniqueException(Exception):
-    """
-    Indicates that a resource is not unique when it should be
-    """
-    pass
--- svenv/blog/models.py
+++ svenv/blog/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-from exceptions import NotUniqueException
 
 
 class Post(models.Model):
@@ -9,7 +8,6 @@ class Post(models.Model):
 
     Posts are written by Django users
     Posts live in a category
-    The combination url_title and category should be unique
     """
     def __str__(self):
         return self.url_title
@@ -20,14 +18,6 @@ class Post(models.Model):
     url_title = models.CharField(max_length=100)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-
-    def save(self, *args, **kwargs):
-        """
-        Override save to prevent duplicate paths
-        """
-        if Post.objects.filter(category=self.category, url_title__icontains=self.url_title):
-            raise NotUniqueException('Another post with url_title %s already exists in category with name %s' % (self.url_title, self.category))
-        super(Post, self).save(*args, **kwargs)
 
 
 class Category(models.Model):
