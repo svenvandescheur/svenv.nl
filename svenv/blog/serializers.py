519529d2c819db9a2067e2b2a85aca6b34d3dbca
Sven van de Scheur <svenvandescheur@gmail.com>
2015-04-25 18:59:09 +0200

    Forgotten file

---
+++ svenv/blog/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers, viewsets
+from models import Category, Image, Post
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
