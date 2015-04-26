87e281fcae53964e0a97aea9a3df13cd9812fe0f
Sven van de Scheur <svenvandescheur@gmail.com>
2015-04-26 17:40:22 +0200

    Add missing file

---
+++ svenv/blog/templatetags/blog_template_helpers.py
@@ -0,0 +1,19 @@
+import dateutil.parser
+from django import template
+import markdown2
+
+
+register = template.Library()
+
+@register.filter
+def datetime(value):
+    try:
+        return dateutil.parser.parse(value)
+    except(AttributeError):
+        return value
+
+
+
+@register.filter
+def markdown(value):
+    return markdown2.markdown(value)
