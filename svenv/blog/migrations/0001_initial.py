1fac677e68789e6436655e8cdb995b7fc1fd4f2c
Sven van de Scheur <svenvandescheur@gmail.com>
2015-05-04 22:43:32 +0200

    Remove old incompatible migrations

--- svenv/blog/migrations/0001_initial.py
+++ svenv/blog/migrations/0001_initial.py
@@ -2,24 +2,44 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Blog',
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Image',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=100)),
+                ('url', models.CharField(max_length=200)),
+                ('width', models.IntegerField()),
+                ('height', models.IntegerField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Post',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=200)),
+                ('url_title', models.CharField(max_length=100)),
                 ('content', models.TextField()),
-                ('date', models.DateTimeField()),
+                ('date', models.DateTimeField(auto_now_add=True)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('category', models.ForeignKey(to='blog.Category')),
+                ('image', models.ForeignKey(to='blog.Image')),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
     ]
