f93bc73054d6ca4ead189c18a06a49518c9791e0
Sven van de Scheur <svenvandescheur@gmail.com>
2015-07-03 01:09:00 +0200

    Allow classes in images

--- svenv/blog/templatetags/formatting.py
+++ svenv/blog/templatetags/formatting.py
@@ -1,5 +1,6 @@
 import dateutil.parser
 from django import template
+from re import sub
 import markdown2
 
 
@@ -13,7 +14,16 @@ def datetime(value):
         return value
 
 
-
 @register.filter
 def markdown(value):
+    value = markdown_class_image(value)
     return markdown2.markdown(value, extras=['fenced-code-blocks'])
+
+
+def markdown_class_image(value):
+    """
+    Extends markdown image syntax with additional class parameter
+    """
+    value = sub(r'!\[(.+?)]\((.+?)\s+["\'](.+)["\']\s["\'](.+?)["\']\)',
+                  r'<img alt="\1" src="\2" title="\3" class="\4" />', value)
+    return value
