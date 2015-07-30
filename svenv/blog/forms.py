fac916fb8508e604c07647e0d5da9e40d3f093ee
Sven van de Scheur <svenvandescheur@gmail.com>
2015-07-30 13:48:51 +0200

    Fixup and style contact form

--- svenv/blog/forms.py
+++ svenv/blog/forms.py
@@ -7,7 +7,7 @@ from smtplib import SMTP
 class ContactForm(forms.Form):
     name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Real or nickname'}))
     email = forms.CharField(validators=[validate_email], widget=forms.TextInput(attrs={'placeholder': 'Kept private'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Feel free to ask me anything'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Feel free to ask me anything', 'cols': 0}))
 
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
