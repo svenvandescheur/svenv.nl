73ea5cea303b58c6cf3b15514333630f51afb46e
Sven van de Scheur <svenvandescheur@gmail.com>
2015-08-11 15:51:38 +0200

    postslist > articleslist

--- svenv/blog/views.py
+++ svenv/blog/views.py
@@ -74,7 +74,7 @@ class CategoryView(BaseBlogView, generic.ListView):
     """
     Shows a list of posts (e.g. home page)
     """
-    context_object_name = 'posts_list'
+    context_object_name = 'articles_list'
     template_name = 'blog/list.html'
 
     def get_name(self):
