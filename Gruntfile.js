936e5926724266893f2644b9e4b2ea59a03bee82
Sven van de Scheur <svenvandescheur@gmail.com>
2015-08-13 13:00:32 +0200

    less before uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,6 +13,19 @@ module.exports = function(grunt) {
       }
     },
 
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          'svenv/blog/static/blog/css/svenv.min.css': ['svenv/blog/static/blog/less/main.less', 'svenv/blog/static/blog/css/fruity.css'],
+        }
+      }
+    },
+
     uglify: {
       options: {
         banner: '/*! Generated: <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %> */\n'
@@ -26,22 +39,10 @@ module.exports = function(grunt) {
         dest: 'svenv/blog/static/blog/js/svenv.min.js'
       }
     },
-    less: {
-      development: {
-        options: {
-          compress: true,
-          yuicompress: true,
-          optimization: 2
-        },
-        files: {
-          'svenv/blog/static/blog/css/svenv.min.css': ['svenv/blog/static/blog/less/main.less', 'svenv/blog/static/blog/css/fruity.css'],
-        }
-      }
-    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.registerTask('default', ['copy', 'uglify', 'less']);
+  grunt.registerTask('default', ['copy', 'less', 'uglify']);
 };
