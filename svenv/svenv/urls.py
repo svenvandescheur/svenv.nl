1f0f2848c4e18bb6e7c5891fb2961c235a94287c
Sven van de Scheur <svenvandescheur@gmail.com>
2015-12-14 19:48:39 +0100

    Compatibility

--- svenv/svenv/urls.py
+++ svenv/svenv/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 
-urlpatterns = patterns('',
-                       url(r'^admin_tools/', include('admin_tools.urls')),
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^', include('blog.urls')),
-                       )
+urlpatterns = [
+    url(r'^admin_tools/', include('admin_tools.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('blog.urls')),
+]
