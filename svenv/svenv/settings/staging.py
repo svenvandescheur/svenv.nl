ccd467b4207de19be5dfd7f56fb038031a71fb67
Sven van de Scheur <svenvandescheur@gmail.com>
2016-09-18 14:31:11 +0200

    Update staging.py

--- svenv/svenv/settings/staging.py
+++ svenv/svenv/settings/staging.py
@@ -3,15 +3,14 @@ from base import *
 
 EXCLUDE_FROM_MINIFYING = ('^sitemap.xml',)
 X_FRAME_OPTIONS = 'ALLOW'
-random = open('/random.txt').read().splitlines()[0]
-SECRET_KEY = random
+pw = open('/pw.txt').read().splitlines()[0]
+SECRET_KEY = pw
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'svenv_nl',
-        'USER': 'admin',
-        'PASSWORD': random,
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'svenv.nl',
+        'USER': 'svenv.nl',
+        'PASSWORD': 'my-secret-password',
+        'HOST': 'postgresql',
     }
 }
